c2df34933ac32b5ad315d138b1f0e163e01f70c0
Agnieszka Lempaszek <agnieszka.lempaszek@student.uj.edu.pl>
2018-11-22 20:17:05 +0100

    Updates

---
+++ precache-manifest.f3d05e6b8387a8047b178901d780fede.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "c86d72737e1264ffe0e6",
+    "url": "/lib-dev-pub/static/css/main.9e77a9b7.chunk.css"
+  },
+  {
+    "revision": "c86d72737e1264ffe0e6",
+    "url": "/lib-dev-pub/static/js/main.c86d7273.chunk.js"
+  },
+  {
+    "revision": "5e02718039c443dfe4aa",
+    "url": "/lib-dev-pub/static/css/1.bbebc8ae.chunk.css"
+  },
+  {
+    "revision": "5e02718039c443dfe4aa",
+    "url": "/lib-dev-pub/static/js/1.5e027180.chunk.js"
+  },
+  {
+    "revision": "0cc2f44f12848ad5296d",
+    "url": "/lib-dev-pub/static/js/runtime~main.0cc2f44f.js"
+  },
+  {
+    "revision": "ba61102b612aa247c604b648a5355306",
+    "url": "/lib-dev-pub/static/media/bookshelf.ba61102b.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/lib-dev-pub/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/lib-dev-pub/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/lib-dev-pub/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/lib-dev-pub/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "080a552e9fe2c1d2fc4ea935c8112848",
+    "url": "/lib-dev-pub/static/media/Byers_IAmEnough.080a552e.jpeg"
+  },
+  {
+    "revision": "2aeaee8839884b9eae1afba3103ca2c9",
+    "url": "/lib-dev-pub/static/media/Caray_SomeoneLikeMe.2aeaee88.jpeg"
+  },
+  {
+    "revision": "63e78e010b62c6794de009af0c2d5216",
+    "url": "/lib-dev-pub/static/media/Humino_IHearTheSunspot.63e78e01.jpeg"
+  },
+  {
+    "revision": "8342b4ca543ef369fe00a45a68a60381",
+    "url": "/lib-dev-pub/static/media/Laymon_Heavy.8342b4ca.jpeg"
+  },
+  {
+    "revision": "b42ed45be12634513e5f3460f2bf6469",
+    "url": "/lib-dev-pub/static/media/Lu_Wildcard.b42ed45b.jpeg"
+  },
+  {
+    "revision": "8e5a7376649bb79b0892d301bc56b2ee",
+    "url": "/lib-dev-pub/static/media/Murakami_KillingCommendatore.8e5a7376.jpeg"
+  },
+  {
+    "revision": "d9a6e5ced8af1ef72850e0bfabc891e0",
+    "url": "/lib-dev-pub/static/media/Sasse_Them.d9a6e5ce.jpeg"
+  },
+  {
+    "revision": "75f7d9d821352f6e940c681a445c0873",
+    "url": "/lib-dev-pub/static/media/XMen5.75f7d9d8.jpeg"
+  },
+  {
+    "revision": "12b0d12853394a0b17f4c4fbe108e510",
+    "url": "/lib-dev-pub/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a782394049f57f6b8640645c61035e3.js"
+  "/lib-dev-pub/precache-manifest.f3d05e6b8387a8047b178901d780fede.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/lib-dev-pub/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
